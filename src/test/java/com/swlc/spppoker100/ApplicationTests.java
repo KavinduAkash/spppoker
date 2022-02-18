b4340a6ec58856e84736dd1b54ac3cc8a68a2d66
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-02-18 16:22:04 +0530

    init starter

---
+++ src/test/java/com/swlc/spppoker100/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swlc.spppoker100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
